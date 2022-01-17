8997be9b2db9df73587dc2d750e157d96b883b51
Lisa-Maria Keck <85275304+Lee-Za@users.noreply.github.com>
2022-01-17 23:49:20 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+API_KEY = ""              # provide your API key
+API_SECRET = ""           # provide your API secret
+ACCESS_TOKEN = ""         # provide your access token
+ACCESS_TOKEN_SECRET = ""  # provide your access token secret
